55b5b7594852cd666c1681bd1df59c27d912caa2
dom0107 <42296228+dom0107@users.noreply.github.com>
2020-02-26 21:33:48 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -71,12 +71,12 @@ namespace WiFiBit {
         writeToSerial("AT+CWJAP=\"" + ssid + "\",\"" + key + "\"", 6000)
     }
 
-        /**
-     * Connect to WiFi network.
+    /**
+     * Connect to WiFi network CLG.
      */
     //% weight=98
     //% blockId="wfb_wifi_on_clg" block="connection WIFI clg"
-    export function connectToWiFiNetwork(ssid: string, key: string): void {
+    export function connectToWiFiNetworkCLG(): void {
         // Connect to AP:
         writeToSerial("AT+CWJAP=\"domiphone\",\"traam2019\"", 6000)
     }
